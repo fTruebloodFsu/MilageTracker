bf62602d71b17c033ff947deb87ed21b44dd1cc2
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-06-22 22:42:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var CreateNewUserRouter = require('./routes/CreateNewUser');
 var CheckAvailabiltyRouter = require('./routes/CheckUserNameAvailability');
 var VerifyUserRouter = require('./routes/verifyUserAndPassword');
@@ -25,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/CreateNewUser', CreateNewUserRouter);
 app.use('/CheckUserNameAvailability', CheckAvailabiltyRouter);
 app.use('/verifyUserAndPassword', VerifyUserRouter);
