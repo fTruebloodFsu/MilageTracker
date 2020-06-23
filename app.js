7f1b46308bebd6baf50d6fac5aa08f3d47344219
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-06-22 21:13:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ var VerifyUserRouter = require('./routes/verifyUserAndPassword');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(logger('dev'));
 app.use(express.json());
