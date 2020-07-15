2511e025528dc3bd08a348405af36d83a2e24713
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-07-15 15:47:17 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 var CreateNewUserRouter = require('./routes/CreateNewUser');
 var CheckAvailabiltyRouter = require('./routes/CheckUserNameAvailability');
 var VerifyUserRouter = require('./routes/verifyUserAndPassword');
+var beginTripRouter = require('./routes/startTrip');
 
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,9 +24,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/CreateNewUser', CreateNewUserRouter);
 app.use('/CheckUserNameAvailability', CheckAvailabiltyRouter);
 app.use('/verifyUserAndPassword', VerifyUserRouter);
+app.use('/startTrip', beginTripRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
