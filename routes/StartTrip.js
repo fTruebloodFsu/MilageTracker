832101cc29d3a8e8534afada38aa1b66ca59db90
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-07-15 15:47:49 -0700

    Add files via upload

---
+++ routes/StartTrip.js
@@ -0,0 +1,30 @@
+var Pool = require('pg').Pool
+var express = require('express');
+var router = express.Router();
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'Colby5892::PG',
+    port: 5432,
+})
+
+router.post('/', function(req, res, next) {
+
+    const { username, tripstart, tripend, triptotal, vehicle, date } = req.body;
+
+    pool.query('INSERT INTO mileageentries(username, tripstart, tripend, triptotal, vehicle, date) VALUES($1, $2, $3, $4, $5, $6)', 
+                [username, tripstart, tripend, triptotal, vehicle, date], 
+                (error,result) => {
+                    if (error) {
+                        throw error
+                    }
+                    res.status(201).send({ message: "Success" })
+                })
+
+
+}
+);
+
+module.exports = router;
