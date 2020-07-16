a55190fc8dd8de462645dfae562774ebaa90c658
fTruebloodFsu <39035131+fTruebloodFsu@users.noreply.github.com>
2020-07-16 14:53:53 -0700

    Add files via upload

---
+++ client/src/LoginRequired.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+const LoginRequired = () => {
+
+    return(
+        <div>
+            <div className="App">
+            <header className="App-header">
+                <img src="LogIn.png" className="App-logo" alt="logo" />
+            </header>
+            </div>
+        </div>
+    );
+}
+
+export default LoginRequired;
